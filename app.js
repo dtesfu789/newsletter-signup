eebca62ffad30b18721965d86a6f5b83b42fe0e8
dtesfu789 <ddytesfu@gmail.com>
2022-05-17 01:03:26 +0200

    Signup process with mailchamp complete

--- app.js
+++ app.js
@@ -10,10 +10,39 @@ app.get("/",function (req,res) {
   res.sendFile(__dirname+"/index.html");
 });
 app.post("/",function(req,res){
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
-  console.log(firstName, lastName, email);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const url = "https://us9.api.mailchimp.com/3.0/lists/AUDIENCE_ID";
+  const dataJSON = JSON.stringify({
+    members:[{
+      email_address:email,
+      status:"subscribed",
+      merge_fields:{
+        FNAME:firstName,
+        LNAME:lastName
+      }
+    }]
+  });
+  const options = {method:"POST",auth:"derejeT:MAILCHIMP_API_KEY"};
+  var request = https.request(url,options,function (response) {
+    if (response.statusCode===200){
+      res.sendFile(__dirname+"/success.html");
+    }else{
+      res.sendFile(__dirname+"/failure.html")
+    }
+    response.on('data',function (data) {
+      var dataParse=JSON.parse(data);
+
+    });
+
+  });
+  request.write(dataJSON);
+  request.end();
+
+});
+app.post("/failure",function (req,res) {
+  res.redirect("/");
 });
 app.listen(port,function () {
   console.log("server open on port 3000");
